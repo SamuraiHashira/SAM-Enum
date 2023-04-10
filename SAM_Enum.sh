bd011a7045314d19dd815936279d269c0b2cc150
Samurai <68142138+SamuraiHashira@users.noreply.github.com>
2023-04-10 03:18:33 +0200

    Update SAM_Enum.sh

--- SAM_Enum.sh
+++ SAM_Enum.sh
@@ -10,20 +10,26 @@ mkdir "$folder_name"
 cd "$folder_name"
 
 # Create new file for subdomains and prompt user for domain name
+
 echo "Enter domain name to enumerate subdomains: "
 read domain_name
 touch "$domain_name.txt"
 
 # Run Subfinder to get subdomains and output to file
+
 echo "Running Subfinder..."
 subfinder -d "$domain_name" >> "$domain_name.txt"
 
 # Use httpx to check which subdomains are working
+
 echo "Checking subdomains with httpx..."
 httpx -l "$domain_name.txt" -silent -o final_Subs.txt
 
 # Use Nuclei to check for vulnerabilities on subdomains with 200 status code
+
 echo "Checking subdomains with Nuclei..."
 cat temp.txt | nuclei  -l final_Subs.txt -o nuclei_output.txt
+
 # Clean up temporary files
+
 rm temp.txt
